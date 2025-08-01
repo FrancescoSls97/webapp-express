0988a0fcde987b722583ae649ee6c21a4e7ab02a
FrancescoSls97 <frankds1997@gmail.com>
2025-08-01 17:49:18 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT
+
+
+//importare asset statici e roter + gestione errori
+
+
+app.listen(PORT, ()=>{
+    console.log('Server is listening');    
+})
+
+app.get('/', (req, res)=>{
+    res.send('HEY LISTEN!! Welcome to the app')
+})
+
+app.get('/movies/:id', (req, res)=>{
+    res.send('Movies List here')
+})
+
+app.get('/movies', (req, res)=>{
+    res.send('One movie here')
+})
