12eead4c7bd635acd6d1bf003037dbd3147635ae
FrancescoSls97 <frankds1997@gmail.com>
2025-08-01 18:05:44 +0200

    connect db

---
+++ database/connection.js
@@ -0,0 +1,21 @@
+//connessione al database
+const mysql = require('mysql2')
+
+const credentials = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+}
+const connection = mysql.createConnection(credentials)
+
+//verifica di connection
+
+connection.connect(err =>{
+    if(err) {
+        throw err
+    }
+    console.info('Database connected')
+})
+
+module.export = connection
