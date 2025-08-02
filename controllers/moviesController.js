b4967b3a3c8cb8e4b108b709f1a1707b74c3ba2d
FrancescoSls97 <frankds1997@gmail.com>
2025-08-02 14:21:54 +0200

    add show function

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -18,8 +18,42 @@ function index(req, res) {
 }
 
 function show(req, res) {
-    res.send('One movie here')
+    const id = req.params.id
+
+    const sql = `SELECT * from movies WHERE id = ?`
+    connection.execute(sql, [id], (err, result) => {
+        if (err) return res.status(500).json({
+            error: true,
+            message: err.message
+        })
+
+        if (result.length === 0) {
+            return res.status(404).json({
+                error: true,
+                message: "Movie not found"
+            })
+        }
+        const movie = result[0]
+
+        const reviewsSql = 'SELECT movies.*, reviews.text FROM movies LEFT JOIN reviews ON movies.id = reviews.movie_id WHERE movies.id = ? '
+
+        connection.execute(reviewsSql,[id], (err, result) => {
+            if (err) return res.status(500).json({
+                error: true,
+                message: err.message
+            })
+
+
+            const movieReviews = result
+
+            movie.reviews = movieReviews
+
+            res.json(movie)
+        })
+
+    })
 }
+
 
 module.exports = {
     index,
